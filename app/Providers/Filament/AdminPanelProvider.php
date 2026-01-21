ee24b929efd6861f42be09bf0df9ba522cbf5fae
Aviad <aviad@zyg.com>
2026-01-22 00:04:59 +0200

    perf: enable Filament SPA mode and optimize production server commands

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,6 +56,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->spa(); // Enable SPA mode for instant navigation
     }
 }
